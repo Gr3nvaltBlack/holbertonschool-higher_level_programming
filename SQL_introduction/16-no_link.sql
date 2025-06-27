b5747ef9d29356e07a790140f1637bcb0a66b837
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-06-27 15:49:15 +0000

    correction v2.2

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- lists all records of the table second_table
 SELECT score, name
 FROM second_table
-WHERE name IS NOT NULL AND name =''
+WHERE name IS NOT NULL AND name !=''
 ORDER BY score DESC;
