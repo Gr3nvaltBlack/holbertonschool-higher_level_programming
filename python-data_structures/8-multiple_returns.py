432638a9359313bda90da65fb7cf283a28c11496
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-05 21:46:33 +0000

    add code for file

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+def multiple_returns(sentence):
+    for idx in range(len(sentence)):
+        if sentence == "":
+            sentence[0] = None
+            return idx, sentence[0]
+    return idx, sentence[0]
