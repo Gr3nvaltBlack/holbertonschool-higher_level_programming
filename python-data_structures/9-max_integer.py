f98bccbe0837c0381ed6ae3ef766b5fa7c3d0959
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-05 22:28:34 +0000

    code for file 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    max_Val = my_list[0]
+    for idx in my_list:
+        if idx > max_Val:
+            max_Val = idx
+    return max_Val
