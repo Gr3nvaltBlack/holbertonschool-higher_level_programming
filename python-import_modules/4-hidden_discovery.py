6ba188fcafa0f47d12d25dda900fbc70a0b0ccda
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-06 22:00:14 +0000

    create file 4 and add code

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    module_name = "hidden_4"
+    hidden_4 = __import__(module_name)
+
+    names = dir(hidden_4)
+
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
