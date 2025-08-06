ba7df4961430d9ae4389580de90a35ab87806d89
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-06 20:46:37 +0000

    correct code 11.1

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    result = 1
-    while b != 0:
-        if b < 0:
-            result *= (1 / a)
-            b += 1
-        else:
-            result *= a
-            b -= 1
-    return result
+    return a ** b
