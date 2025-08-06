cf26bf5ae2ace200e3b901561a1d3249f260019f
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-06 13:07:47 +0000

    correction 8.3

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -5,6 +5,6 @@ def multiple_returns(sentence):
     if idx == 0:
         Char = None
     else:
-         Char = sentence[0]
-    tuple_val = (idx, sentence[0])
+        Char = sentence[0]
+    tuple_val = (idx, Char)
     return tuple_val
