5d714cfeaf520e5108b3b0107c2e87f29b57768e
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-06 12:58:16 +0000

    correction 8.1

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    for idx in range(len(sentence)):
-        if sentence == "":
-            sentence[0] = None
-            return idx, sentence[0]
-    return idx, sentence[0]
+    idx = len(sentence)
+    if idx == 0:
+        sentence[0] = None
+    tuple_val = (idx, sentence[0])
+    return tuple_val
