dd2ad1bccd73ddf8a192462121f4d7b7d3e5b429
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-07 17:24:31 +0000

    fix code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,6 @@
 
 def read_file(filename=""):
     """Open a file and print its content to stdout"""
-    with open(filename, "r", encoding= "utf-8") as f:
-        print(f.read(), end="")
+    with open(filename, encoding= "utf-8") as f:
+        read_data = f.read()
+    print(read_data, end="")
