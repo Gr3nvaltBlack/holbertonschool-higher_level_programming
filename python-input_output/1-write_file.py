bd2434d24f6aaca16d02e2d9c92c3610d3a5d2e2
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-07 17:32:53 +0000

    fix space

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """Write a string to a UTF8 text file and return number of characters"""
-    with open(filename, "w", encoding= "utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
