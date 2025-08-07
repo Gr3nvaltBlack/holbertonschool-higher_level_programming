d689b6d6e59cddc5cbc58d4052e7d2dd51fba138
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-07 18:58:02 +0000

    fix and comment

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
+'''Module 11-student'''
 
 
 class Student:
-    """Define a student with first name, last name, and age"""
-
+    '''
+    class Student defined by:
+        first_name
+        last_name
+        age
+    '''
     def __init__(self, first_name, last_name, age):
-        """Initialize student attributes"""
+        '''Instantiation of Student class'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return a dictionary representation of the instance"""
-        if isinstance(attrs, list) and all(isinstance(el, str) for el in attrs):
-            return {key: self.__dict__[key], for key in attrs
-                    if key in self.__dict__}
-        return self.__dict__
+        '''
+        Public method that retrieves
+            a dictionnary representation of Student instance
+        If attrs is a list of strings,
+            only attribute names contained in this list must be retrieved.
+        Otherwise, all attributes must be retrieved.
+        '''
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key,
+                    value in self.__dict__.items() if key in attrs}
 
     def reload_from_json(self, json):
-        """Replace attributes of the instance from a dictionary"""
+        '''
+        Replaces all attributes of the Student instance
+        '''
         for key, value in json.items():
             setattr(self, key, value)
