b229bbb761ac894678791f4c321584eaadf20071
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-07 14:57:48 +0000

    correct code

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
-count = 0
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
     for index in range(x):
         try:
             print("{:d}".format(my_list[index]), end="")
