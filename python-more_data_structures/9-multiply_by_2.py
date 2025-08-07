1cef66f4a30fe2733740818a9c01afb0a133ae5c
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-07 13:31:10 +0000

    correct code 9.3

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    return {key: value * 2 for key, value in a_dictionary.item()}
+    new_dictionary = {}
+    for key, value in a_dictionary.items():
+        new_dictionary[key] = value * 2
+    return new_dictionary
