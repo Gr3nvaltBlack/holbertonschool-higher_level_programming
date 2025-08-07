9f3972a8f9365487d99bff538089f9d9a3dc3c2c
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-07 10:54:15 +0000

    correct code and pycodestyle

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [for row in matrix [for idx in row]]
+    new_matrix = [[idx ** 2 for idx in row] for row in matrix]
     return new_matrix
