b832b7d71c7b077774153eec7d45ec09693474bc
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-07 13:29:47 +0000

    code for file 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    new_dict = {key: value * 2 for key, value in a_dictionary.item()}
-    return new_dict
+    return {key: value * 2 for key, value in a_dictionary.item()}
