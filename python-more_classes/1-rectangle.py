1badcf8e6d567e3b808898dfe1aba69364b66291
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-07 14:25:00 +0000

    correct code 1.4

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,7 +3,11 @@
 Module for a Rectangle
 '''
 
+
 class Rectangle:
+    '''
+    Class for a Rectangle
+    '''
     def __init__(self, width=0, height=0):
         '''
         Instantiation of a Rectangle
@@ -17,24 +21,38 @@ class Rectangle:
 
     @property
     def width(self):
+        '''Private instance attribute that returns the width of a Rectangle'''
         return self.__width
 
     @width.setter
     def width(self, value):
+        '''
+        Private instance attribute that sets the width of a Rectangle
+
+        Args: value (int): Value given to set the width of a Rectangle
+        '''
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
+        '''Private instance attribute that returns the height of a Rectangle'''
         return self.__height
 
     @height.setter
     def height(self, value):
+        '''
+        Private instance attribute that sets the height of a Rectangle
+
+        Args: value (int): Value given to set the height of a Rectangle
+        '''
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
