f34f8c4e82408fe645283fed50dd294e812c6961
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-07 18:51:55 +0000

    comment code 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+'''Module 10-student'''
 
 
 class Student:
-    """Define a student with first name, last name, and age"""
-
+    '''
+    class Student defined by:
+        first_name
+        last_name
+        age
+    '''
     def __init__(self, first_name, last_name, age):
         """Initialize student attributes"""
         self.first_name = first_name
@@ -11,7 +16,13 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return a dictionary representation of the instance"""
+        '''
+        Public method that retrieves
+            a dictionnary representation of Student instance
+        If attrs is a list of strings,
+            only attribute names contained in this list must be retrieved.
+        Otherwise, all attributes must be retrieved.
+        '''
         if isinstance(attrs, list) and all(isinstance(el, str) for el in attrs):
             return {key: self.__dict__[key], for key in attrs
                     if key in self.__dict__}
